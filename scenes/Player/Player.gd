d8fa637f2acefb2d4251d549a0c9c0505a456aff
Kelvin2themoon <2ukelvin@gmail.com>
2024-01-24 20:05:46 -0800

    where we was but now there is states

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,28 +1,88 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+#old variables
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#jump and move vars [g]round [a]ir
+enum MoveState { on_ground, jumping, falling, fast_fall, cyote}
+var player_state = MoveState.on_ground
+
+@export var g_top_speed		= 200
+@export var g_forward_speed =  20 
+@export var g_reverse_speed =  10
+@export var g_decel 		=  10 #ground friction
+
+@export var a_top_speed 	=  100
+@export var a_forward_speed =  10
+@export var a_reverse_speed =  10
+@export var a_decel 		=  300 #air friction
+
+@export var jump_strength 	= -400
+@export var grav_normal		=  980 #normal gravity, like when move off a ledge
+@export var grav_on_jump 	=  980 #project settings = 980
+@export var grav_on_fall 	=  980*2 #increase grav on release jump button
+@export var max_fall_speed  =  800
 
 
+#PHYSICS STEP
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#friction check
+	
+	#state check
+	if is_on_floor():
+		player_state = MoveState.on_ground
+	
+	#check state
+	match player_state:
+		MoveState.on_ground:
+			print("on ground")
+			#jump
+			if Input.is_action_just_pressed("jump"):
+				print("jump")
+				player_state = MoveState.jumping
+				velocity.y = jump_strength
+				 
+		MoveState.jumping:
+			velocity.y = move_toward(velocity.y, max_fall_speed, grav_on_jump*delta) 
+		MoveState.falling:
+			print("falling")
+			velocity.y = move_toward(velocity.y, max_fall_speed, grav_normal)
+			
+	#move left and right
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction :
+		print("moving")
+		velocity.x = move_toward(velocity.x, direction*g_top_speed, g_forward_speed)
+		print(velocity.x)
+	else :
+		velocity.x = move_toward(velocity.x, 0, 10)
+	
 	move_and_slide()
+	
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += grav_on_jump * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump"):
+		##velocity.y = JUMP_VELOCITY
+		#if is_on_floor() :
+			#velocity.y = jump_strength
+		##else :
+		##	velocity.y += grav_on_jump
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("move_left", "move_right")
+#
+	#if direction :
+		#velocity.x = move_toward(velocity.x, direction*g_top_speed, g_forward_speed)
+		#print(velocity.x)
+#
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, 10)
+#
+	#move_and_slide()
+
